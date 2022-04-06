9b8392b88ce6015eb0d4ae18549a8004c9352d66
vajihemansoori <vajihe.mansoori76@gmail.com>
2022-04-06 15:50:37 +0430

    add new code

---
+++ js/details.js
@@ -0,0 +1,24 @@
+const id = new URLSearchParams(window.location.search).get("id");
+const container = document.querySelector(".details");
+const deleteBtn = document.querySelector(".delete");
+const renderDetails = async () => {
+  const res = await fetch("http://localhost:3000/posts/" + id);
+  if (!res.ok) {
+    window.location.replace("/");
+  }
+  const post = await res.json();
+  let img = document.createElement("img");
+  img.src = post.img;
+  const template = `
+    <h1>${post.title}</h1>
+    <p>${post.body}</p>`;
+  container.innerHTML = template;
+  container.append(img);
+};
+deleteBtn.addEventListener("click", async () => {
+  const res = await fetch("http://localhost:3000/posts/" + id, {
+    method: "DELETE",
+  });
+  window.location.replace("/index.html");
+});
+window.addEventListener("DOMContentLoaded", renderDetails);
